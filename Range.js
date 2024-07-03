118825facbbe759da5ec0c1d6847c7d1b5845137
Maria Eduarda Lopes <161601222+lopesduda@users.noreply.github.com>
2024-07-03 10:32:45 -0300

    exercício 9

---
+++ Range.js
@@ -0,0 +1,11 @@
+function range(min, max){
+    var min = 3; 
+    var max = 9;
+    var vetInt=[]; 
+    for (let i = min; i <= max; i++){
+        vetInt.push(i); 
+    }
+    
+    console.log(vetInt); 
+}
+range();
