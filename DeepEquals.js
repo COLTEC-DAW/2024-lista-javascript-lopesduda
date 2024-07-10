d82a53197871c25f7bc19b2daf774cd7488ad7f1
Maria Eduarda Lopes <161601222+lopesduda@users.noreply.github.com>
2024-07-10 08:26:16 -0300

    exercício 13

---
+++ DeepEquals.js
@@ -0,0 +1,17 @@
+function deepEquals(obj1, obj2){
+    return obj1 == obj2;
+
+}
+
+function Testa(){
+    let obj1 = 10; 
+    let obj2 = 12; 
+    
+    if (obj1 == obj2){
+        console.log("true"); 
+    }else{
+        console.log("false"); 
+    }
+}
+
+Testa(); 
